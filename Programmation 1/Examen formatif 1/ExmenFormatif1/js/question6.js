a920f801950fb844e04bcbb9f7a6d62e844f05c2
GabrielBoivin <boivin.gabriel@carrefour.cegepvicto.ca>
2019-09-17 14:22:51 -0400

    Design Web/

---
+++ Programmation 1/Examen formatif 1/ExmenFormatif1/js/question6.js
@@ -0,0 +1,8 @@
+var prixDeBase = 100;
+var compositionBaton = "érable" || "bouleau jaune" || "mérisier";
+var manche = "gros manche" || "manche normal";
+var longueurBaton;
+var prixFinalSansLivraison;
+var prixFinalAvecLivraison;
+var prixFinal;
+var prixLivraison = prixFinal*0.15;
